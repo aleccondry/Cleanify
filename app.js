2397b9f39572ae26ee1645546f33233f0e6957d2
aleccondry <aleccondry@gmail.com>
2018-11-26 10:32:36 -0500

    made the authorization url

--- app.js
+++ app.js
@@ -5,12 +5,23 @@ const Spotify = require('spotify-web-api-node');
 
 var spotify = new Spotify({
   clientId: '366e8b5ceafa4a548f5be611eaffe8ab',
-  clientSecret: process.env.ClientSecret
+  clientSecret: process.env.ClientSecret,
+  redirectUri: 'https://example.com/callback'
 });
+
+var scopes = ['user-read-private', 'user-read-email'],
+  redirectUri,
+  clientId,
+  state = Math.random().toString(36).substring(2, 15);
+
+
+//create authorization url
+var authorizeURL = spotify.createAuthorizeURL(scopes, state);
+console.log(authorizeURL);
 
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(8080, function(){
     console.log('Server running on 8080...');
-    console.log(process.env.ClientSecret);
+
 });
