d6a1efe4f232c8edbf1515c76b5be372a7a38222
mbkarle <mkarle2178@gmail.com>
2018-11-26 20:40:47 -0500

    cleaned up merge conflict

--- app.js
+++ app.js
@@ -23,8 +23,4 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(8080, function(){
     console.log('Server running on 8080...');
-<<<<<<< HEAD
-
-=======
->>>>>>> slight css cleanup and a gitignore
 });
