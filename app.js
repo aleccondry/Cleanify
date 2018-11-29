e940189080390fed4a980e201844d0c4afaaebe4
mbkarle <mkarle2178@gmail.com>
2018-11-29 09:42:49 -0500

    authorize url

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const port = 3000;
-
+var fs = require('fs');
 const Spotify = require('spotify-web-api-node');
 
 var spotify = new Spotify({
   clientId: '366e8b5ceafa4a548f5be611eaffe8ab',
   clientSecret: process.env.ClientSecret,
-  redirectUri: 'https://example.com/callback'
+  redirectUri: 'http://localhost:8080'
 });
 
 var scopes = ['user-read-private', 'user-read-email'],
@@ -17,8 +17,10 @@ var scopes = ['user-read-private', 'user-read-email'],
 
 //create authorization url
 var authorizeURL = spotify.createAuthorizeURL(scopes, state);
-console.log(authorizeURL);
 
+fs.writeFileSync("package.json", 'var package = {"authorizeURL":"'+authorizeURL+'"}', 'utf-8');
+
+console.log(fs.readFileSync("package.json").toString());
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(8080, function(){
