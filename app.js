27c91c8f58c109111294155cb1719d0ff7b5980b
aleccondry <aleccondry@gmail.com>
2018-12-06 12:15:08 -0500

    push definitions

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const http = require('http');
 var fs = require('fs');
-const express = require('express');
+var express = require('express');
+var request = require('request');
 const ejs = require('ejs');
-var app = express();
 var url = require('url');
+var cors = require('cors');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+var app = express();
+
+var client_id = '366e8b5ceafa4a548f5be611eaffe8ab'; // Your client id
+var client_secret = process.env.ClientSecret; // Your secret
+var redirect_uri = 'http://localhost:8080/'; // Your redirect uri
 
 var SpotifyWebApi = require('spotify-web-api-node');
 
 var spotifyApi = new SpotifyWebApi({
-  clientId: '366e8b5ceafa4a548f5be611eaffe8ab',
-  clientSecret: process.env.ClientSecret,
-  redirectUri: 'http://localhost:8080/callback/'
+  clientId: client_id,
+  clientSecret: client_secret,
+  redirectUri: redirect_uri
 });
 
 var scopes = ['user-read-private', 'user-read-email'],
@@ -27,39 +35,82 @@ fs.writeFileSync("pass.json", 'var package = {"authorizeURL":"'+authorizeURL+'"}
 console.log(fs.readFileSync("pass.json").toString());
 console.log(authorizeURL);
 
-function fullUrl(req){
-  return url.format({
-    protocol: req.protocol,
-    host: req.get('host'),
-    path: req.originalUrl
-  });
-}
+// app.use(express.static(__dirname + '/public'))
+//    .use(cors())
+//    .use(cookieParser());
 
-app.get('/callback', function(req, res){
-  var code = req.query.code || null;
-  var storedState = req.state;
-  var state = req.query.state || null;
+app.configure(function () {
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+// app.use(express.session({
+//     store: sessionStore,
+//     secret: 'secret',
+//     key: 'express.sid'
+// }));
+app.use(app.router); //Now router comes first and will be executed before static middleware
+app.use(express.static(__dirname + '/public'));
+});
 
-  console.log('hello world');
 
-  if (state !== storedState || state === null) {
-    res.redirect('/#' + querystring.stringify({error: 'state_mismatch'}));
-  } else {
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {'Authorization': 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64')) },
-      form: {
-        grant_type: 'refresh_token',
-        refresh_token: refresh_token
-      },
-      JSON: true
-    };
-  }
+app.get('/', function(req, res) {
+  console.log("L");
+  res.send("hello world");
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  // var code = req.query.code || null;
+  // var state = req.query.state || null;
+  //
+  // if (state === null || state !== storedState) {
+  //   res.redirect('/#' +
+  //     querystring.stringify({
+  //       error: 'state_mismatch'
+  //     }));
+  // } else {
+  //   var authOptions = {
+  //     url: 'https://accounts.spotify.com/api/token',
+  //     form: {
+  //       code: code,
+  //       redirect_uri: redirect_uri,
+  //       grant_type: 'authorization_code'
+  //     },
+  //     headers: {
+  //       'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  //     },
+  //     json: true
+  //   };
+  //
+  //   request.post(authOptions, function(error, response, body) {
+  //     if (!error && response.statusCode === 200) {
+  //
+  //       var access_token = body.access_token,
+  //           refresh_token = body.refresh_token;
+  //
+  //       var options = {
+  //         url: 'https://api.spotify.com/v1/me',
+  //         headers: { 'Authorization': 'Bearer ' + access_token },
+  //         json: true
+  //       };
+  //
+  //       // use the access token to access the Spotify Web API
+  //       request.get(options, function(error, response, body) {
+  //         console.log(body);
+  //       });
+  //
+  //       // we can also pass the token to the browser to make requests from there
+  //       res.redirect('/#' +
+  //         querystring.stringify({
+  //           access_token: access_token,
+  //           refresh_token: refresh_token
+  //         }));
+  //     } else {
+  //       res.redirect('/#' +
+  //         querystring.stringify({
+  //           error: 'invalid_token'
+  //         }));
+  //     }
+  //   });
+  // }
 });
 
 var connect = require('connect');
@@ -67,5 +118,5 @@ var serveStatic = require('serve-static');
 
 connect().use(serveStatic(__dirname)).listen(8080, function(){
     console.log('Server running on 8080...');
-
+    
 });
