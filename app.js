8e8d999d1e04dddb6401f0989879cac98e388766
Matthew <mkarle2178@gmail.com>
2018-12-25 01:57:33 +0000

    Implemented Authorization Code Workflow
    This cleans up the 400 status errors.
    Also cleaned up code.
    
    NOTE: 400 errors can happen if secret key is wrong.

--- app.js
+++ app.js
@@ -1,51 +1,52 @@
+// IMPORTS
 const http = require('http');
-var fs = require('fs');
-
-var cors = require('cors');
-var querystring = require('querystring');
-var cookieParser = require('cookie-parser');
-var request = require('request');
+const fs = require('fs');
+const cors = require('cors');
+const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
+const request = require('request');
 const express = require('express');
-var app = express();
 const path = require('path');
+const SpotifyWebApi = require('spotify-web-api-node');
 
-
+// GLOBALS
+var app = express();
 var client_id = '366e8b5ceafa4a548f5be611eaffe8ab'; // Your client id
-var client_secret = fs.readFileSync("env.txt").toString();
-console.log("Secret: " + client_secret); // Your secret
-var redirect_uri = 'http://localhost:8080/callback'; // Your redirect uri
+var client_secret = fs.readFileSync("env.txt").toString().trim();
+var redirect_uri = 'http://cleanify.mooo.com/callback'; // Your redirect uri
 var refresh_token = null;
 
-var SpotifyWebApi = require('spotify-web-api-node');
-
+// Initialize the API Wrapper
 var spotifyApi = new SpotifyWebApi({
   clientId: client_id,
   clientSecret: client_secret,
   redirectUri: redirect_uri
 });
 
-var scopes = ['user-read-private', 'user-read-email'],
-  redirectUri,
-  clientId,
-  storedState = Math.random().toString(36).substring(2, 15);
-
-
-//create authorization url
+// Create authorization url
+var scopes = ['user-read-private', 'user-read-email'];
+var storedState = Math.random().toString(36).substring(2, 15);
 var authorizeURL = spotifyApi.createAuthorizeURL(scopes, storedState);
 
+// Some debugging info
+console.log("Secret: " + client_secret); // Your secret
 fs.writeFileSync("public/pass.json", 'var package = {"authorizeURL":"'+authorizeURL+'"}', 'utf-8');
-
 console.log(fs.readFileSync("public/pass.json").toString());
 console.log(authorizeURL);
 
+// Serve up all html
 app.use(express.static('public'))
+
+// Default route (send index.html)
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/index.html'));
-  console.log("sent");
-})
-// app.use(express.static(__dirname));
+  console.log("home accessed");
+});
+
+// Callback route (redirect for API)
 app.get('/callback', function(req, res){
-  console.log("l");
+  var authOptions;
+  console.log(req.url)
   var code = req.query.code || null;
   // var storedState = req.state;
   var state = req.query.state || null;
@@ -53,17 +54,19 @@ app.get('/callback', function(req, res){
   if (state !== storedState || state === null) {
     res.redirect('/#' + querystring.stringify({error: 'state_mismatch'}));
   } else {
-    var authOptions = {
+    authOptions = {
       url: 'https://accounts.spotify.com/api/token',
       form: {
         grant_type: 'authorization_code',
         code: code,
-        redirect_uri: redirect_uri,
+        redirect_uri: redirect_uri/*,
         client_id: client_id,
-        client_secret: client_secret
+        client_secret: client_secret*/
       },
-      headers: {'Authorization': 'Basic ' + new Buffer.from((client_id + ':' + client_secret), 'base64')},
-      JSON: true
+      headers: {
+        'Authorization': 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64')
+      },
+      json: true
     };
   }
   console.log('url: ' + authOptions.url);
@@ -72,30 +75,26 @@ app.get('/callback', function(req, res){
   console.log('redirect_uri: ' + authOptions.form.redirect_uri);
   console.log('headers: ' + authOptions.headers.Authorization);
 
-  // {url:'https://accounts.spotify.com/api/token', form:{grant_type:'authorization_code', code:authOptions.code, redirect_uri: redirect_uri, client_id: client_id, client_secret: client_secret}}
-  request.post({url:'https://accounts.spotify.com/api/token', form:{grant_type:'authorization_code', code:authOptions.code, redirect_uri: redirect_uri, client_id: client_id, client_secret: client_secret}}, function(error, response, body){
-    if (!error && response.statusCode == 200) {
-      var access_token = body.access_token;
-      var refresh_token = body.refresh_token;
-      console.log('access_token: ' + access_token);
-      console.log('refresh_token: ' + refresh_token);
+  // Get the authorization access token
+  spotifyApi.authorizationCodeGrant(code).then(
+    function(data) {
+      console.log('The token expires in ' + data.body['expires_in']);
+      console.log('The access token is ' + data.body['access_token']);
+      console.log('The refresh token is ' + data.body['refresh_token']);
 
-      var options = {
-        url: 'https://api.spotify.com/v1/me',
-        headers: { 'Authorization': 'Bearer ' + access_token },
-        json: true
-      };
-    }else {
-      console.log(error);
+      // Set the access token on the API object to use it in later calls
+      spotifyApi.setAccessToken(data.body['access_token']);
+      spotifyApi.setRefreshToken(data.body['refresh_token']);
+
+      console.log('WE MADE IT!');
+    },
+    function(err) {
+      console.log('Something went wrong!', err);
     }
-  });
-  res.redirect('/');
-  }
-)
+  );
 
-app.listen(8080);
-// var connect = require('connect');
-// var serveStatic = require('serve-static');
-// connect().use(serveStatic(__dirname)).listen(8080, function(){
-//     console.log('Server running on 3000...');
-// });
+  // Send it back home
+  res.redirect('/');
+});
+
+app.listen(3000);
